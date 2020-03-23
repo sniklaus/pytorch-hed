3d695d6fb812d334aad18966f3a495394dbefba2
Simon Niklaus <simon.niklaus@outlook.com>
2020-03-23 09:43:24 -0700

    no message

--- run.py
+++ run.py
@@ -36,14 +36,14 @@ class Network(torch.nn.Module):
 	def __init__(self):
 		super(Network, self).__init__()
 
-		self.moduleVggOne = torch.nn.Sequential(
+		self.netVggOne = torch.nn.Sequential(
 			torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False),
 			torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False)
 		)
 
-		self.moduleVggTwo = torch.nn.Sequential(
+		self.netVggTwo = torch.nn.Sequential(
 			torch.nn.MaxPool2d(kernel_size=2, stride=2),
 			torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False),
@@ -51,7 +51,7 @@ class Network(torch.nn.Module):
 			torch.nn.ReLU(inplace=False)
 		)
 
-		self.moduleVggThr = torch.nn.Sequential(
+		self.netVggThr = torch.nn.Sequential(
 			torch.nn.MaxPool2d(kernel_size=2, stride=2),
 			torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False),
@@ -61,7 +61,7 @@ class Network(torch.nn.Module):
 			torch.nn.ReLU(inplace=False)
 		)
 
-		self.moduleVggFou = torch.nn.Sequential(
+		self.netVggFou = torch.nn.Sequential(
 			torch.nn.MaxPool2d(kernel_size=2, stride=2),
 			torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False),
@@ -71,7 +71,7 @@ class Network(torch.nn.Module):
 			torch.nn.ReLU(inplace=False)
 		)
 
-		self.moduleVggFiv = torch.nn.Sequential(
+		self.netVggFiv = torch.nn.Sequential(
 			torch.nn.MaxPool2d(kernel_size=2, stride=2),
 			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
 			torch.nn.ReLU(inplace=False),
@@ -81,75 +81,75 @@ class Network(torch.nn.Module):
 			torch.nn.ReLU(inplace=False)
 		)
 
-		self.moduleScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
-		self.moduleScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
-		self.moduleScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
-		self.moduleScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
-		self.moduleScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
 
-		self.moduleCombine = torch.nn.Sequential(
+		self.netCombine = torch.nn.Sequential(
 			torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
 			torch.nn.Sigmoid()
 		)
 
-		self.load_state_dict(torch.load(__file__.replace('run.py', 'network-' + arguments_strModel + '.pytorch')))
+		self.load_state_dict({ strKey.replace('module', 'net'): tenWeight for strKey, tenWeight in torch.load(__file__.replace('run.py', 'network-' + arguments_strModel + '.pytorch')).items() })
 	# end
 
-	def forward(self, tensorInput):
-		tensorBlue = (tensorInput[:, 0:1, :, :] * 255.0) - 104.00698793
-		tensorGreen = (tensorInput[:, 1:2, :, :] * 255.0) - 116.66876762
-		tensorRed = (tensorInput[:, 2:3, :, :] * 255.0) - 122.67891434
+	def forward(self, tenInput):
+		tenBlue = (tenInput[:, 0:1, :, :] * 255.0) - 104.00698793
+		tenGreen = (tenInput[:, 1:2, :, :] * 255.0) - 116.66876762
+		tenRed = (tenInput[:, 2:3, :, :] * 255.0) - 122.67891434
 
-		tensorInput = torch.cat([ tensorBlue, tensorGreen, tensorRed ], 1)
+		tenInput = torch.cat([ tenBlue, tenGreen, tenRed ], 1)
 
-		tensorVggOne = self.moduleVggOne(tensorInput)
-		tensorVggTwo = self.moduleVggTwo(tensorVggOne)
-		tensorVggThr = self.moduleVggThr(tensorVggTwo)
-		tensorVggFou = self.moduleVggFou(tensorVggThr)
-		tensorVggFiv = self.moduleVggFiv(tensorVggFou)
+		tenVggOne = self.netVggOne(tenInput)
+		tenVggTwo = self.netVggTwo(tenVggOne)
+		tenVggThr = self.netVggThr(tenVggTwo)
+		tenVggFou = self.netVggFou(tenVggThr)
+		tenVggFiv = self.netVggFiv(tenVggFou)
 
-		tensorScoreOne = self.moduleScoreOne(tensorVggOne)
-		tensorScoreTwo = self.moduleScoreTwo(tensorVggTwo)
-		tensorScoreThr = self.moduleScoreThr(tensorVggThr)
-		tensorScoreFou = self.moduleScoreFou(tensorVggFou)
-		tensorScoreFiv = self.moduleScoreFiv(tensorVggFiv)
+		tenScoreOne = self.netScoreOne(tenVggOne)
+		tenScoreTwo = self.netScoreTwo(tenVggTwo)
+		tenScoreThr = self.netScoreThr(tenVggThr)
+		tenScoreFou = self.netScoreFou(tenVggFou)
+		tenScoreFiv = self.netScoreFiv(tenVggFiv)
 
-		tensorScoreOne = torch.nn.functional.interpolate(input=tensorScoreOne, size=(tensorInput.shape[2], tensorInput.shape[3]), mode='bilinear', align_corners=False)
-		tensorScoreTwo = torch.nn.functional.interpolate(input=tensorScoreTwo, size=(tensorInput.shape[2], tensorInput.shape[3]), mode='bilinear', align_corners=False)
-		tensorScoreThr = torch.nn.functional.interpolate(input=tensorScoreThr, size=(tensorInput.shape[2], tensorInput.shape[3]), mode='bilinear', align_corners=False)
-		tensorScoreFou = torch.nn.functional.interpolate(input=tensorScoreFou, size=(tensorInput.shape[2], tensorInput.shape[3]), mode='bilinear', align_corners=False)
-		tensorScoreFiv = torch.nn.functional.interpolate(input=tensorScoreFiv, size=(tensorInput.shape[2], tensorInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreOne = torch.nn.functional.interpolate(input=tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreTwo = torch.nn.functional.interpolate(input=tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreThr = torch.nn.functional.interpolate(input=tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
 
-		return self.moduleCombine(torch.cat([ tensorScoreOne, tensorScoreTwo, tensorScoreThr, tensorScoreFou, tensorScoreFiv ], 1))
+		return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
 	# end
 # end
 
-moduleNetwork = None
+netNetwork = None
 
 ##########################################################
 
-def estimate(tensorInput):
-	global moduleNetwork
+def estimate(tenInput):
+	global netNetwork
 
-	if moduleNetwork is None:
-		moduleNetwork = Network().cuda().eval()
+	if netNetwork is None:
+		netNetwork = Network().cuda().eval()
 	# end
 
-	intWidth = tensorInput.shape[2]
-	intHeight = tensorInput.shape[1]
+	intWidth = tenInput.shape[2]
+	intHeight = tenInput.shape[1]
 
 	assert(intWidth == 480) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
 	assert(intHeight == 320) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
 
-	return moduleNetwork(tensorInput.cuda().view(1, 3, intHeight, intWidth))[0, :, :, :].cpu()
+	return netNetwork(tenInput.cuda().view(1, 3, intHeight, intWidth))[0, :, :, :].cpu()
 # end
 
 ##########################################################
 
 if __name__ == '__main__':
-	tensorInput = torch.FloatTensor(numpy.array(PIL.Image.open(arguments_strIn))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+	tenInput = torch.FloatTensor(numpy.array(PIL.Image.open(arguments_strIn))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
 
-	tensorOutput = estimate(tensorInput)
+	tenOutput = estimate(tenInput)
 
-	PIL.Image.fromarray((tensorOutput.clamp(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(arguments_strOut)
+	PIL.Image.fromarray((tenOutput.clamp(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(arguments_strOut)
 # end
